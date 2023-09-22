1b2fd5d1a08e5be73ebf03f6a2fdf4bba18fb98f
ThEditor <theditormcpe@gmail.com>
2023-09-22 17:52:04 +0530

    feat: mock agency data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,48 @@ import ResRequest, { IResRequest } from '@/components/ResRequest';
 
 const data: IResRequest[] = [
   {
-    name: 'Looking for Meds',
-    details: 'Insulin needed',
-    location: [1, 2],
+    name: 'NDRF - Tamil Nadu',
+    details: 'National Disaster Response Force in Tamil Nadu',
+    location: [12.9716, 77.5946],
+  },
+  {
+    name: 'TN SDMA',
+    details: 'Tamil Nadu State Disaster Management Authority',
+    location: [13.0827, 80.2707],
+  },
+  {
+    name: 'Indian Red Cross - Coimbatore',
+    details: "Indian Red Cross Society's Coimbatore Chapter",
+    location: [11.0168, 76.9558],
+  },
+  {
+    name: 'CARE India - Chennai Office',
+    details: "CARE India's office in Chennai",
+    location: [13.0827, 80.2707],
+  },
+  {
+    name: 'Save the Children - Chennai',
+    details:
+      "Save the Children's office in Chennai, providing child-focused disaster relief.",
+    location: [13.0827, 80.2707],
+  },
+  {
+    name: 'World Food Programme - Tamil Nadu',
+    details:
+      "WFP's initiatives in Tamil Nadu to provide food assistance during disasters.",
+    location: [13.0827, 80.2707],
+  },
+  {
+    name: 'ActionAid India - Madurai',
+    details:
+      "ActionAid's presence in Madurai, offering disaster relief and community support.",
+    location: [9.9252, 78.1198],
+  },
+  {
+    name: 'ShelterBox - Tamil Nadu',
+    details:
+      "ShelterBox's efforts to provide emergency shelter and tools in Tamil Nadu.",
+    location: [11.0168, 76.9558],
   },
 ];
 
