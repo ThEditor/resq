89c1c011ce0f6b7fad5e19eb1f4eaf33459deb45
ThEditor <theditormcpe@gmail.com>
2023-09-22 17:26:38 +0530

    feat: request card

---
+++ src/components/ResRequest.tsx
@@ -0,0 +1,50 @@
+import clsxm from '@/lib/clsxm';
+import timeToRelativeTime from '@/lib/timeToRelativeTime';
+
+export interface IResRequest {
+  name: string;
+  details: string;
+  location: [number, number];
+  time: Date;
+}
+
+export interface ResRequestProps extends IResRequest {
+  className?: string;
+}
+
+export default function ResRequest({
+  className,
+  name,
+  details,
+  location,
+  time,
+}: ResRequestProps) {
+  return (
+    <div className='relative flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md'>
+      <div className='p-6'>
+        {}
+        <h5 className='text-blue-gray-900 mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal antialiased'>
+          {name}
+        </h5>
+        <p className='block font-mono text-xs leading-relaxed text-gray-700 text-inherit antialiased'>
+          Lat: {location[0]} Long: {location[1]}
+        </p>
+        <p className='block font-sans text-base font-light leading-relaxed text-inherit antialiased'>
+          {details}
+        </p>
+      </div>
+      <div className='flex items-center gap-4 p-6 pt-0'>
+        <button
+          className='select-none rounded-lg bg-green-500 px-6 py-3 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none'
+          type='button'
+          data-ripple-light='true'
+        >
+          Accept
+        </button>
+        <div className='block font-sans text-base font-light leading-relaxed text-inherit antialiased'>
+          {timeToRelativeTime(time)}
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,27 @@
-import { useRouter } from 'next/router';
-import * as React from 'react';
+import ResRequest, { IResRequest } from '@/components/ResRequest';
 
-import TextButton from '@/components/buttons/TextButton';
-import Seo from '@/components/Seo';
+const data: IResRequest[] = [
+  {
+    name: 'Looking for Meds',
+    details: 'Insulin needed',
+    location: [1, 2],
+    time: new Date(),
+  },
+];
 
 export default function HomePage() {
-  const router = useRouter();
   return (
-    <>
-      <Seo />
-      <main className='flex h-screen flex-col items-center justify-center gap-4'>
-        <h1>ResQ</h1>
-        <div className='flex w-1/4 gap-4'>
-          <TextButton
-            onClick={() => router.push('/login')}
-            className='flex-grow'
-          >
-            Log In
-          </TextButton>
-          <TextButton
-            onClick={() => router.push('/signup')}
-            className='flex-grow'
-          >
-            Sign Up
-          </TextButton>
-        </div>
-      </main>
-    </>
+    <div className='mt-5 flex flex-col items-center justify-center gap-4'>
+      <h1>Requests</h1>
+      {data.map((v, i) => (
+        <ResRequest
+          key={i}
+          name={v.name}
+          details={v.details}
+          location={v.location}
+          time={v.time}
+        />
+      ))}
+    </div>
   );
 }
