6bb71275b674f871eee5216ca320b1dac4937c08
ThEditor <theditormcpe@gmail.com>
2023-09-23 05:22:59 +0530

    feat: migrate to marker requests

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -2,29 +2,36 @@ import clsxm from '@/lib/clsxm';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css';
 import 'leaflet-defaulticon-compatibility';
-import React, { Dispatch, SetStateAction } from 'react';
+import React, { Dispatch, SetStateAction, useEffect, useState } from 'react';
 
 import { MapContainer, Marker, TileLayer, useMapEvents } from 'react-leaflet';
 import L, { LeafletMouseEventHandlerFn } from 'leaflet';
+import axios from '@/lib/api/axios';
+import { IAgency } from '@/components/requests/AgencySelector';
 
 export interface MapProps {
   className?: string;
   markerPosition?: [number, number];
   requestMarkerLocation?: [number, number];
   setRequestMarkerLocation: Dispatch<SetStateAction<[number, number]>>;
+  handleOnSelect: (agency: IAgency) => void;
   containerRef: React.MutableRefObject<null>;
   onClick: (loc: [number, number]) => void;
 }
 
 const Map = React.forwardRef(
-  ({
-    className,
-    containerRef,
-    markerPosition,
-    requestMarkerLocation,
-    setRequestMarkerLocation,
-    ...rest
-  }: MapProps) => {
+  (
+    {
+      className,
+      containerRef,
+      markerPosition,
+      requestMarkerLocation,
+      setRequestMarkerLocation,
+      handleOnSelect,
+      ...rest
+    }: MapProps,
+    ref
+  ) => {
     const redMarkerIcon = new L.Icon({
       iconUrl:
         'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
@@ -33,6 +40,18 @@ const Map = React.forwardRef(
       popupAnchor: [1, -34],
       shadowSize: [41, 41],
     });
+    useEffect(() => {
+      axios.get('/agencies').then((x) => {
+        const list: IAgency[] = x.data;
+        for (const agency of list) {
+          L.marker(agency.coordinates)
+            .addTo(containerRef.current!)
+            .on('mousedown', () => {
+              handleOnSelect(agency);
+            });
+        }
+      });
+    }, []);
     return (
       <MapContainer
         {...rest}
@@ -40,13 +59,13 @@ const Map = React.forwardRef(
         className={clsxm(className, 'h-screen w-full')}
         center={markerPosition}
         zoom={15}
+        doubleClickZoom={false}
         scrollWheelZoom={true}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
         />
-        {markerPosition && <Marker position={markerPosition}></Marker>}
         {requestMarkerLocation && (
           <Marker
             position={requestMarkerLocation}
@@ -69,7 +88,7 @@ const MapClickHandler = ({
   onMapClick: LeafletMouseEventHandlerFn;
 }) => {
   const map = useMapEvents({
-    click: onMapClick,
+    dblclick: onMapClick,
   });
 
   return null; // This component doesn't render anything, it's just for event handling
--- src/components/requests/MyRequests.tsx
+++ src/components/requests/MyRequests.tsx
@@ -1,77 +1,7 @@
-import AgencySelector from '@/components/requests/AgencySelector';
-import { useState } from 'react';
-import Modal from 'react-modal';
-
 export interface MyRequestsProps {
   className?: string;
-  requestMarkerLocation?: [number, number];
 }
 
-export default function MyRequests({
-  className,
-  requestMarkerLocation,
-}: MyRequestsProps) {
-  const [modalIsOpen, setIsOpen] = useState(false);
-
-  function openModal() {
-    setIsOpen(true);
-  }
-
-  function afterOpenModal() {}
-
-  function closeModal() {
-    setIsOpen(false);
-  }
-
-  const [selectedAgencies, setSelectedAgencies] = useState<string[]>([]);
-
-  const handleSubmit = (e: any) => {};
-  return (
-    <div>
-      <button
-        onClick={() => openModal()}
-        className='flex items-center space-x-2 rounded-full bg-white px-4 py-2 text-black hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-white'
-      >
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          className='h-6 w-6'
-          fill='none'
-          stroke='currentColor'
-          viewBox='0 0 24 24'
-        >
-          <path
-            stroke-linecap='round'
-            stroke-linejoin='round'
-            stroke-width='2'
-            d='M12 4v16m8-8H4'
-          ></path>
-        </svg>
-        <span>Add</span>
-      </button>
-      <Modal
-        isOpen={modalIsOpen}
-        onAfterOpen={afterOpenModal}
-        onRequestClose={closeModal}
-      >
-        <button onClick={closeModal}>close</button>
-        <form
-          className='flex flex-col gap-4 text-black'
-          onSubmit={handleSubmit}
-        >
-          <h3>Create a new Request</h3>
-          <input placeholder='Name' />
-          <input placeholder='Address' />
-          <label htmlFor='tags' className='block text-sm font-medium'>
-            Select Agency Type {'(Hold Control/Shift for multiple)'}:
-          </label>
-          <AgencySelector />
-          <label className='block text-sm font-medium'>
-            Request uses the current marker location{' '}
-            {'(Click on Map to select location)'}.
-          </label>
-          <button type='submit'>Create</button>
-        </form>
-      </Modal>
-    </div>
-  );
+export default function MyRequests({}: MyRequestsProps) {
+  return <div></div>;
 }
